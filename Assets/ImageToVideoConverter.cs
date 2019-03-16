dfb3240556996ba87f1ff51ee0d12ea7e2c17a11
Shunsuke Saito <shunsuke15vb@gmail.com>
2019-03-16 20:16:38 +0900

    LocalFileの動画をストリーミング再生

---
+++ Assets/ImageToVideoConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class ImageToVideoConverter : MonoBehaviour
+{
+    private RawImage image;
+    private VideoPlayer video;
+
+    void Start()
+    {
+        image = GetComponent<RawImage>();
+        video = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (video.isPrepared)
+        {
+            image.texture = video.texture;
+        }
+    }
+}
